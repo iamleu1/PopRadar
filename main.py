9cedfd764ece4950a2657756f94bca3825d9201a
Alessandro <alessandrolimacosta5@gmail.com>
2026-06-11 18:12:31 -0300

    aprimorando a posicao

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ url = "https://www.billboard.com/charts/artist-100/"
 
 listas_artistas = minerar_links(url)
 
-c=0
+
 if listas_artistas[:10]:
     for a in listas_artistas:
-        print(f"{c}º - {a['Nome']}")
-        c = c + 1
+        print(f"{listas_artistas}º - {a['Nome']}")
 
     df = pd.DataFrame(listas_artistas)
+    df.index = df.index + 1
     arquivo_existe = os.path.exists('historico_billboard.csv')
     df.to_csv('historico_billboard.csv', mode='a', header=not arquivo_existe, encoding='utf-8-sig')
 else: 
